fa806f8b2d5a108069c5e63dfe03a256e62f5529
Alsymon <alsymon1038@gmail.com>
2023-11-11 16:20:08 +0800

    Added crime list, crime analyst, and background color

--- src/app/crimeanalyst/crimeanalyst.component.ts
+++ src/app/crimeanalyst/crimeanalyst.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { guest_card_data, topnav_data, guest_sidenav_data } from 'src/shared_data/dashboard_data';
 
 @Component({
   selector: 'app-crimeanalyst',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./crimeanalyst.component.scss']
 })
 export class CrimeanalystComponent {
-
+  cardData = guest_card_data;
+  guest = topnav_data;
+  sidenav = guest_sidenav_data;
 }
--- src/app/crimelist/crimelist.component.ts
+++ src/app/crimelist/crimelist.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { guest_card_data, guest_sidenav_data, topnav_data } from 'src/shared_data/dashboard_data';
 
 @Component({
   selector: 'app-crimelist',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./crimelist.component.scss']
 })
 export class CrimelistComponent {
-
+  cardData = guest_card_data;
+  guest = topnav_data;
+  sidenav = guest_sidenav_data;
 }
