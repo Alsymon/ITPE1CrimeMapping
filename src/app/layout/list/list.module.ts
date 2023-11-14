4d5d5b006910527a64f9efe4240b7c8d28afc51b
Alsymon <alsymon1038@gmail.com>
2023-11-14 19:20:47 +0800

    a

--- src/app/layout/list/list.module.ts
+++ src/app/layout/list/list.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ListRoutingModule } from './list-routing.module';
 import { SharedComponentsModule } from 'src/app/components/shared-components.module';
 import { ListComponent } from './list.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -11,7 +12,8 @@ import { ListComponent } from './list.component';
   imports: [
     CommonModule,
     ListRoutingModule,
-    SharedComponentsModule
+    SharedComponentsModule,
+    FormsModule
   ]
 })
 export class ListModule { }
--- src/app/layout/user-management/user-management.component.ts
+++ src/app/layout/user-management/user-management.component.ts
@@ -10,8 +10,38 @@ export class UserManagementComponent implements OnInit {
   cardData = admin_card_data;
   admin = admin_topnav_data;
   admin_sidenav = admin_sidenav_data;
+  buttonText: string = "I'm inside typescript";
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  passwordInputType = 'password';
+  users: { username: string; email: string; password: string, passwordHidden: boolean }[] = [];
 
-  ngOnInit(): void {
-    //console.log ("This is card data", this.cardData)
-   }
+  addUser() {
+    if (this.username && this.email && this.password) {
+      this.users.push({ username: this.username, email: this.email, password: this.password,passwordHidden: true});
+      this.username = '';
+      this.email = '';
+      this.password = '';
+      this.passwordInputType = 'password';
+
+      localStorage.setItem('userManagementData', JSON.stringify(this.users));
+    }
+  }
+
+  deleteUser(index: number) {
+    this.users.splice(index, 1);
+
+    localStorage.setItem('userManagementData', JSON.stringify(this.users));
+  }
+  togglePasswordVisibility(index: number) {
+    this.users[index].passwordHidden = !this.users[index].passwordHidden;
+    this.passwordInputType = this.users.some(user => !user.passwordHidden) ? 'text' : 'password';
+  }
+  ngOnInit() {
+    const storedData = localStorage.getItem('userManagementData');
+    if (storedData) {
+      this.users = JSON.parse(storedData);
+    }
+  }
 }
